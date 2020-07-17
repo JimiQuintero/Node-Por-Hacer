69d1129f2daa346a7a1c9ba3eeb1b191e228e9f2
Jimi Quintero <jjimiq@gmail.com>
2020-07-17 00:24:10 -0500

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+
+
+let listadoPorHacer = [];
+
+const guardaDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo guardar', err)
+    });
+}
+
+const cargarDB = () => {
+
+    try {
+
+        listadoPorHacer = require('../db/data.json');
+
+    } catch (error) {
+
+        listadoPorHacer = [];
+    }
+
+
+
+}
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porhacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porhacer);
+
+    guardaDB();
+
+    return porhacer;
+}
+
+const getListado = () => {
+
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => {
+        return tarea.descripcion === descripcion;
+    });
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardaDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion;
+    });
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardaDB();
+        return true;
+    }
+}
+
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
